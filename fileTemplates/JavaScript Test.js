405958d7ce944f8dc3e3ebf4eb4cd9a31c553b46
Peet <peter.goddard@contactpartners.com>
2017-03-07 14:48:27 +0000

    WS-171.3691.10 <petergoddard@Pete-Desktop Create baseRefactoring.xml, markdown.xml, AMD JavaScript File.js, IntelliLang.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, Default copy.xml, other.xml, power.mode.ii.xml, terminal.xml, Default.xml, JavaScript Test.js, AngularJS .xml, applicationLibraries.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, Peet.icls, code.style.schemes.xml, colors.scheme.xml, Angular2.xml, github_settings.xml, Tomorrow Night.icls, JavaScript File.js, abbreviations.xml, Default.xml, laf.xml, Sunshine.icls, TypeScript File.ts, intentionSettings.xml, Zen XSL.xml, Default _1_.xml, web-browsers.xml, vcs.xml, ObsidianBlade (Darcula).icls, editor.codeinsight.xml, Wombat.icls, React Component.js, editor.xml, usageView.xml, debugger.xml, Majestic.xml

---
+++ fileTemplates/JavaScript Test.js
@@ -0,0 +1,7 @@
+describe('', () => {
+
+  it('works', () => {
+    expect(true).toBeTruthy();  
+  });
+
+});
---
+++ fileTemplates/React Component.js
@@ -0,0 +1,17 @@
+import React, { PropTypes, Component } from 'react';
+
+export default class ${NAME} extends Component {
+
+  static propTypes = {
+  };
+
+  static defaultProps = {
+  };
+
+  state = {
+  };
+
+  render() {
+    return <div/>; 
+  }
+}
